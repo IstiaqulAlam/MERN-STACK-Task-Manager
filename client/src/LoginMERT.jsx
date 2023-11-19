788e17e5f7f215814fbe7eff54b9da2e4f85dd95
Joseph Finfrock <70216057+4804032140@users.noreply.github.com>
2023-11-19 15:04:42 -0500

    Update LoginMERT.jsx

--- client/src/LoginMERT.jsx
+++ client/src/LoginMERT.jsx
@@ -40,9 +40,6 @@ function LoginMERT() {
           <div id="loginNotice"></div>
         </div>
       </div>
-      <div className="mountain left-mountain"></div>
-      <div className="mountain right-mountain"></div>
-      <div className="sun"></div>
       <script src="LoginScript.js"></script>
     </>
   );
